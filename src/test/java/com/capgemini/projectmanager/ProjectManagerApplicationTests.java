3c99ca389ad2c3eabd417cc16a8371df9bf18125
adilnaib <naib-mohammed.adil@capgemini.com>
2024-11-27 10:47:57 +0530

    Initial

---
+++ src/test/java/com/capgemini/projectmanager/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
